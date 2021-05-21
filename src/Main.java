3f5c740ebfe79c86bb79df6cc2caa25e4183919b
Leo Tsant <leo.tsa@hotmail.com>
2021-05-21 22:28:01 +0300

    added roomHoursOccupied and reserveRoom methods in Event Class

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,51 @@ public class Main {
 		// test christos methods
 		// ==============================================================================================================
 		// dummy data
-		String[] testCourses = {"Software Engineering", "Advanced Microprocessors"};
+		String[] testCourses = { "Software Engineering", "Advanced Microprocessors" };
 		Department ceid = new Department("Computer Engineering & Informatics Department");
-		Student testStudent1 = new Student("Christos", "Pelekis", "cpelekis", "123456789", 0, 2017, testCourses, 0, ceid);
-		Student testStudent2 = new Student("Leonidas", "Tsantarliotis", "ltsantarliotis", "123456789", 1, 2017, testCourses, 0, ceid);		
-		Student testStudent3 = new Student("Georgios", "Thomas", "gthomas", "123456789", 2, 2017, testCourses, 0, ceid);		
+		Student testStudent1 = new Student("Christos", "Pelekis", "cpelekis", "123456789", 0, 2017, testCourses, 0,
+				ceid);
+		Student testStudent2 = new Student("Leonidas", "Tsantarliotis", "ltsantarliotis", "123456789", 1, 2017,
+				testCourses, 0, ceid);
+		Student testStudent3 = new Student("Georgios", "Thomas", "gthomas", "123456789", 2, 2017, testCourses, 0, ceid);
 		Scanner testScanner = new Scanner();
 		LocalDateTime startDate = LocalDateTime.of(2021, Month.MAY, 21, 16, 44, 0); // 2021, May, 21, 16:45:00
 		LocalDateTime endDate = LocalDateTime.of(2021, Month.MAY, 21, 18, 0, 0); // 2021, May, 21, 18:00:00
-		Student[] participants = {testStudent1, testStudent2};
+		LocalDateTime startDate2 = LocalDateTime.of(2021, Month.MAY, 21, 15, 0, 0);
+		LocalDateTime endDate2 = LocalDateTime.of(2021, Month.MAY, 21, 17, 0, 0);
+		LocalDateTime startDate3 = LocalDateTime.of(2021, Month.MAY, 20, 9, 0, 0);
+		LocalDateTime endDate3 = LocalDateTime.of(2021, Month.MAY, 20, 11, 0, 0);
+		LocalDateTime startDate4 = LocalDateTime.of(2021, Month.MAY, 21, 9, 0, 0);
+		LocalDateTime endDate4 = LocalDateTime.of(2021, Month.MAY, 21, 11, 0, 0);
+		Student[] participants = { testStudent1, testStudent2 };
 		Teacher testTeacher = new Teacher("Nikos", "Nikolopoulos", "nnikolopoulos", "123456789", testCourses, ceid);
 		Room testRoom = new Room(20, true, ceid, "B4");
 		Course testCourse = new Course("CEID1234", "Advanced Microprocessors Lab", 2, 4, true, true, ceid);
-		Student[] testAbsentLog = {testStudent1, testStudent2};		
-		Lab testEvent = new Lab("CEID1234", startDate, endDate, participants, testTeacher, testRoom,
-				testCourse, testAbsentLog, 15);
-		
-		// validation is random, expected students are testStudent1 and 2 
-		if (testScanner.isValidStudent(testStudent3) && testScanner.isExpectedStudent(testStudent3, testEvent)) { // validation is random
+		Student[] testAbsentLog = { testStudent1, testStudent2 };
+		Lab testEvent = new Lab("CEID1234", startDate, endDate, participants, testTeacher, testRoom, testCourse,
+				testAbsentLog, 15);
+
+		// validation is random, expected students are testStudent1 and 2
+		if (testScanner.isValidStudent(testStudent3) && testScanner.isExpectedStudent(testStudent3, testEvent)) { // validation
+																													// is
+																													// random
 			testScanner.isLateStudent(testEvent);
-		}	
-		
-		testEvent.findCourseLog("CEID1234",startDate,endDate,testRoom);
+		}
+
+		testEvent.findCourseLog("CEID1234", startDate, endDate, testRoom);
 		// ==============================================================================================================
 
-		
+		// test leo's methods
+		// <==============================================================================================================
+		Event Lesson1 = new Event("1", startDate, endDate, testAbsentLog, testTeacher, testRoom, testCourse);
+		Event Lesson2 = new Event("2", startDate2, endDate2, testAbsentLog, testTeacher, testRoom, testCourse);
+		Event Lesson3 = new Event("3", startDate3, endDate3, testAbsentLog, testTeacher, testRoom, testCourse);
+		Event[] events = { Lesson1, Lesson2, Lesson3 };
+		System.out.println("Hours occupied: " + Lesson1.roomHoursOccupied(testRoom, "FRIDAY", events));
+
+		Lesson1.reserveRoom(testRoom, startDate4, endDate4);
+		System.out.println("Hours occupied: " + Lesson1.hoursOccupiedFriday);
+		// ==============================================================================================================>
 	}
 
 }
