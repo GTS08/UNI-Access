23774cfedef377e0d0bdfd02b95f0b0b35b1a120
George Thomas <georgethomas806@gmail.com>
2021-06-06 22:41:40 +0300

    minor change

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// Test functionality of the system
+
 		// ==============================================================================================================
 		// dummy data
 		String[] testCourses = { "Software Engineering", "Advanced Microprocessors" };
